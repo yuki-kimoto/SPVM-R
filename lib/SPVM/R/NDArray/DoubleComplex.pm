2901bfca875c6e4606f1f6abd7290367eafba3fd
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-11-20 10:59:45 +0900

    Add R::NDArray::(Float|Double)Complex.

---
+++ lib/SPVM/R/NDArray/DoubleComplex.pm
@@ -0,0 +1,45 @@
+package SPVM::R::NDArray::DoubleComplex;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::R::NDArray::DoubleComplex - Short Description
+
+=head1 Description
+
+The R::NDArray::DoubleComplex class of L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use R::NDArray::DoubleComplex;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
