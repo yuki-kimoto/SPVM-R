ae6ce4dd5ee2617a1f789d9f78e37353cad0d779
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-06-25 16:27:34 +0900

    Add Matrix classes.

---
+++ lib/SPVM/R/Op/Matrix/DoubleComplex.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+extern "C" {
+
+static const char* FILE_NAME = "R/Op/Matrix/DoubleComplex.cpp";
+
+int32_t SPVM__R__Op__Matrix__DoubleComplex__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+}
