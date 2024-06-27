c04e4aa35485ed80f2c8c6a3445cfd68df31ed20
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-06-27 10:23:18 +0900

    Add t method.

--- lib/SPVM/R/OP/Matrix/Double.cpp
+++ lib/SPVM/R/OP/Matrix/Double.cpp
@@ -90,4 +90,42 @@ int32_t SPVM__R__OP__Matrix__Double___solve(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   return 0;
 }
+
+int32_t SPVM__R__OP__Matrix__Double___t(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  void* ret_ndarray_ref = stack[0].oval;
+  
+  int32_t* ret_row_ref = stack[1].iref;
+  
+  int32_t* ret_column_ref = stack[2].iref;
+  
+  void* obj_x_data = stack[3].oval;
+  double* x_data = env->get_elems_double(env, stack, obj_x_data);
+  
+  int32_t x_row = stack[4].ival;
+  
+  int32_t x_column = stack[5].ival;
+  
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> x_matrix = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>>(x_data, x_row, x_column);
+  
+  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> ret_matrix = x_matrix;
+  
+  ret_matrix.transposeInPlace();
+  
+  int32_t ret_length = ret_matrix.rows() * ret_matrix.cols();
+  void* obj_ret_data = env->new_double_array(env, stack, ret_length);
+  
+  double* ret_data = env->get_elems_double(env, stack, obj_ret_data);
+  
+  memcpy(ret_data, ret_matrix.data(), sizeof(double) * ret_length);
+  
+  env->set_elem_object(env, stack, ret_ndarray_ref, 0, ret_data);
+  
+  *ret_row_ref = ret_matrix.rows();
+  
+  *ret_column_ref = ret_matrix.cols();
+  
+  return 0;
+}
+
 }
