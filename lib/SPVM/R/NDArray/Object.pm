4c8b6964fd6db633da9103b586a02839e1a9fa09
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-07-11 10:00:46 +0900

    Fix the before commit.

--- lib/SPVM/R/NDArray/Object.pm
+++ lib/SPVM/R/NDArray/Object.pm
@@ -6,31 +6,87 @@ package SPVM::R::NDArray::Object;
 
 =head1 Name
 
-SPVM::R::NDArray::Object - Short Description
+SPVM::R::NDArray::Object - N-Dimensional Array of object Type.
 
 =head1 Description
 
-The R::NDArray::Object class in L<SPVM> has methods to do someting.
+R::NDArray::Object class in L<SPVM> represents n-dimensional array of object type.
 
 =head1 Usage
 
   use R::NDArray::Object;
-
-=head1 Details
-
-
+  
+  my $ndarray = R::NDArray::Object->new({data => [(object)1, 2, 3, 4, 5, 6], dim => [3, 2]});
 
 =head1 Super Class
 
 L<R::NDArray|SPVM::R::NDArray>
 
+=head1 Field
+
+=head2 data
+
+C<method data : object[] ();>
+
+Same as L<R::NDArray#data|SPVM::R::NDArray/"data"> method, but the return type is different.
+
 =head1 Class Methods
 
+C<static method new : L<R::NDArray::Object|SPVM::R::NDArray::Object> ($options : object[] = undef);>
 
+Creates a new L<R::NDArray::Object|SPVM::R::NDArray::Object> and returns it.
+
+This method calls L<R::NDArray#init|SPVM::R::NDArray/"init"> method given the options $options.
 
 =head1 Instance Methods
 
+=head2 create_default_data
 
+C<method create_default_data : object[] ($length : int = 0);>
+
+Creates a default data given the length $length and returns it.
+
+=head2 elem_to_string
+
+C<method elem_to_string : string ($data : object[], $data_index : int);>
+
+Converts an element $data at index $data_index to a string and returns it.
+
+=head2 elem_assign
+
+C<method elem_assign : void ($dist_data : object[], $dist_data_index : int, $src_data : object[], $src_data_index : int);>
+
+Assigns the element $src_data at index $src_data_index to the element $dist_data at index $dist_data_index.
+
+=head2 elem_clone
+
+C<method elem_clone : void ($dist_data : object[], $dist_data_index : int, $src_data : object[], $src_data_index : int);>
+
+Copies the element $src_data at index $src_data_indext to the element $dist_data at index $dist_data_index.
+
+=head2 elem_cmp
+
+C<method elem_cmp : int ($data : object[], $a_data_index : int, $b_data_index : int);>
+
+Compares the element $data at index $a_data_index and the element $data at index $b_data_index and returns the result.
+
+=head2 clone
+
+C<method clone : L<R::NDArray::Object|SPVM::R::NDArray::Object> ($shallow : int = 0);>
+
+Same as L<R::NDArray#clone|SPVM::R::NDArray/"clone"> method, but the return type is different.
+
+=head2 slice
+
+C<method slice : L<R::NDArray::Object|SPVM::R::NDArray::Object> ($asix_indexes_product : L<R::NDArray::Int|SPVM::R::NDArray::Int>[]);>
+
+Same as L<R::NDArray#slice|SPVM::R::NDArray/"slice"> method, but the return type is different.
+
+=head2 to_int_ndarray
+
+C<method to_int_ndarray : L<R::NDArray::Int|SPVM::R::NDArray::Int> ();>
+
+Converts this n-dimensional array to a n-dimensional array of L<R::NDArray::Int|SPVM::R::NDArray::Int> and returns it.
 
 =head1 Copyright & License
 
