b5b007eaebc1c72e158033a91ca3fffca11200e6
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-07-11 10:44:49 +0900

    Improve R::NDArray::Double.

--- lib/SPVM/R/NDArray/Double.pm
+++ lib/SPVM/R/NDArray/Double.pm
@@ -82,6 +82,30 @@ C<method slice : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($asix_indexes_p
 
 Same as L<R::NDArray#slice|SPVM::R::NDArray/"slice"> method, but the return type is different.
 
+=head2 to_int_ndarray
+
+C<method to_int_ndarray : L<R::NDArray::Int|SPVM::R::NDArray::Int> ();>
+
+Converts this n-dimensional array to a n-dimensional array of L<R::NDArray::Int|SPVM::R::NDArray::Int> and returns it.
+
+=head2 to_long_ndarray
+
+C<method to_long_ndarray : L<R::NDArray::Long|SPVM::R::NDArray::Long> ();>
+
+Converts this n-dimensional array to a n-dimensional array of L<R::NDArray::Long|SPVM::R::NDArray::Long> and returns it.
+
+=head2 to_float_ndarray
+
+C<method to_float_ndarray : L<R::NDArray::Float|SPVM::R::NDArray::Float> ();>
+
+Converts this n-dimensional array to a n-dimensional array of L<R::NDArray::Float|SPVM::R::NDArray::Float> and returns it.
+
+=head2 to_double_complex_ndarray
+
+C<method to_double_complex_ndarray : L<R::NDArray::DoubleComplex|SPVM::R::NDArray::DoubleComplex> ();>
+
+Converts this n-dimensional array to a n-dimensional array of L<R::NDArray::DoubleComplex|SPVM::R::NDArray::DoubleComplex> and returns it.
+
 =head1 Copyright & License
 
 Copyright (c) 2024 Yuki Kimoto
