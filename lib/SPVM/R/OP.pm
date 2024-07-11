b9e9a3894fa43b2e296c2bac4c1392894d7d0fbb
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-07-12 08:53:31 +0900

    Remove sort_asc, sort_desc from R::DataFrame and add sort.

--- lib/SPVM/R/OP.pm
+++ lib/SPVM/R/OP.pm
@@ -6,52 +6,60 @@ package SPVM::R::OP;
 
 =head1 Name
 
-SPVM::R::OP - Short Description
+SPVM::R::OP - N-Dimensional Array Operations
 
 =head1 Description
 
-The R::OP class in L<SPVM> has methods to do someting.
+The R::OP class in L<SPVM> has methods for operations for L<R::NDArray|SPVM::R::NDArray>.
 
 =head1 Usage
 
   use R::OP;
 
-=head1 Details
-
-
-
-=head1 Inheritance
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
-
 =head1 Class Methods
 
 
+=head1 Related Modules
 
-=head1 Instance Methods
+=over 2
 
+=item * L<R::OP|SPVM::R::OP>
 
+=item * L<R::OP::Byte|SPVM::R::OP::Byte>
 
+=item * L<R::OP::Short|SPVM::R::OP::Short>
 
-=head1 Repository
+=item * L<R::OP::Int|SPVM::R::OP::Int>
 
+=item * L<R::OP::Long|SPVM::R::OP::Long>
 
+=item * L<R::OP::Float|SPVM::R::OP::Float>
 
-=head1 Author
+=item * L<R::OP::Double|SPVM::R::OP::Double>
 
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=item * L<R::OP::FloatComplex|SPVM::R::OP::FloatComplex>
+
+=item * L<R::OP::DoubleComplex|SPVM::R::OP::DoubleComplex>
+
+=item * L<R::OP::AnyObject|SPVM::R::OP::AnyObject>
+
+=item * L<R::OP::String|SPVM::R::OP::String>
+
+=item * L<R::OP::StringBuffer|SPVM::R::OP::StringBuffer>
+
+=item * L<R::OP::Time::Piece|SPVM::R::OP::Time::Piece>
+
+=back
+
+=head1 See Also
+
+=over 2
+
+=item * L<R::NDArray|SPVM::R::NDArray>
+
+=item * L<R|SPVM::R>
+
+=back
 
 =head1 Copyright & License
 
