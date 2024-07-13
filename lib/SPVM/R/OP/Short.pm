c6fb53b2a8aff3aef41dbd3b2b9b71ddf275ea5d
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-07-13 09:27:07 +0900

    Improve docs for R::OP::Short.

--- lib/SPVM/R/OP/Short.pm
+++ lib/SPVM/R/OP/Short.pm
@@ -6,52 +6,41 @@ package SPVM::R::OP::Short;
 
 =head1 Name
 
-SPVM::R::OP::Short - Short Description
+SPVM::R::OP::Short - N-Dimensional Array Operations for R::NDArray::Short
 
 =head1 Description
 
-The R::OP::Short class in L<SPVM> has methods to do someting.
+The R::OP::Short class in L<SPVM> has methods for n-dimensional array operations for L<R::NDArray::Short|SPVM::R::NDArray::Short>.
 
 =head1 Usage
 
-  use R::OP::Short;
-
-=head1 Details
-
-
-
-=head1 Inheritance
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
-
-=head1 Fields
-
-
+  use R::OP::Short as SOP;
+  
+  my $ndarray_scalar = SOP->c((short)1);
+  
+  my $ndarray_vector = SOP->c([(short)1, 2, 3]);
+  
+  my $ndarray = SOP->c([(short)1, 2, 3, 4, 5, 6], [3, 2]);
+  
+  my $ndarray2 = SOP->c($ndarray);
 
 =head1 Class Methods
 
+=head2 c
 
+C<static method c : L<R::NDArray::Short|SPVM::R::NDArray::Short> ($data : object of L<Short|SPVM::Short>|short[]|L<R::NDArray::Short|SPVM::R::NDArray::Short>, $dim : int[] = undef);>
 
-=head1 Instance Methods
+=head1 See Also
 
+=over 2
 
+=item * L<R::NDArray::Short|SPVM::R::NDArray::Short>
 
+=item * L<R::NDArray|SPVM::R::NDArray>
 
-=head1 Repository
+=item * L<R|SPVM::R>
 
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=back
 
 =head1 Copyright & License
 
