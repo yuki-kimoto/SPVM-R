19976e00d62321879e8297e2db54cb1bc11dd60a
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-08-13 09:18:20 +0900

    Add docs for R::OP::Int class.
      [Document Improvement]
        * Add docs for R::OP::Int class.

--- lib/SPVM/R/OP/Double.pm
+++ lib/SPVM/R/OP/Double.pm
@@ -34,7 +34,7 @@ C<static method c : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($data : obje
 
 C<static method add : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $y_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs addition C<+> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs addition C<+> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -48,7 +48,7 @@ The dim field of $x_ndarray must be equal to the dim field of $y_ndarray. Otherw
 
 C<static method sub : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $y_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs subtraction C<-> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs subtraction C<-> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -62,7 +62,7 @@ The dim field of $x_ndarray must be equal to the dim field of $y_ndarray. Otherw
 
 C<static method mul : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $y_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs multiplication C<*> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs multiplication C<*> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -76,7 +76,7 @@ The dim field of $x_ndarray must be equal to the dim field of $y_ndarray. Otherw
 
 C<static method scamul : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $scalar_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs multiplication C<*> operation on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array and the n-dimensional array $scalar_ndarray at data index 0 to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs multiplication C<*> operation on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array and the n-dimensional array $scalar_ndarray at data index 0 to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -90,7 +90,7 @@ The n-dimensional array $x_ndarray must be defined. Otherwise, an exception is t
 
 C<static method div : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $y_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs division C</> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs division C</> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -104,7 +104,7 @@ The dim field of $x_ndarray must be equal to the dim field of $y_ndarray. Otherw
 
 C<static method scadiv : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $scalar_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs division C</> operations on the n-dimensional array $scalar_ndarray at data index 0 and each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs division C</> operations on the n-dimensional array $scalar_ndarray at data index 0 and each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -118,7 +118,7 @@ The n-dimensional array $x_ndarray must be defined. Otherwise, an exception is t
 
 C<static method neg : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs negation C<-> operation on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs negation C<-> operation on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -128,7 +128,7 @@ The n-dimensional array $x_ndarray must be defined. Otherwise, an exception is t
 
 C<static method abs : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs L<Math#fabs|SPVM::Math/"fabs"> method on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs L<Math#fabs|SPVM::Math/"fabs"> method on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -138,7 +138,7 @@ The n-dimensional array $x_ndarray must be defined. Otherwise, an exception is t
 
 C<static method eq : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>, $y_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs numeric comparison C<==> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs numeric comparison C<==> operation on each element of the n-dimensional array $x_ndarray and $y_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
@@ -238,7 +238,7 @@ C<static method seq_length : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($be
 
 C<static method sin : L<R::NDArray::Double|SPVM::R::NDArray::Double> ($x_ndarray : L<R::NDArray::Double|SPVM::R::NDArray::Double>);>
 
-Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object for a return value, performs L<Math#sin|SPVM::Math/"sin"> method on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
+Creates a new L<R::NDArray::Double|SPVM::R::NDArray::Double> object of the same dimensions as the n-dimensional array $x_ndarray for a return value, performs L<Math#sin|SPVM::Math/"sin"> method on each element of the n-dimensional array $x_ndarray, and sets each element of the new n-dimensional array to the each operation result, and returns the new n-dimensional array.
 
 Exceptions:
 
